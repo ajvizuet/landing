a2003016299207c37b261b2458f7cf75902e23f5
= <=>
2024-12-01 11:07:01 -0500

    Update Suscribers form && coffee makers

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const databaseURL = 'https://landing-d0e76-default-rtdb.firebaseio.com/collection.json'; 
+const databaseURL = 'https://landing-d0e76-default-rtdb.firebaseio.com/collection.json';
 
 let sendData = () => {
     let form = document.getElementById('form');
@@ -14,79 +14,84 @@ let sendData = () => {
         },
         body: JSON.stringify(data)
     })
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.statusText}`);
-        }
-        return response.json();
-    })
-    .then(result => {
-        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
-        form.reset()
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(result => {
+            alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+            form.reset()
 
-        getData();
-    })
-    .catch(error => {
-        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
-    });
+            getData();
+        })
+        .catch(error => {
+            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        });
 }
 
 let getData = async () => {
     try {
         const response = await fetch(databaseURL);
 
-        if(!response.ok) {
+        if (!response.ok) {
             alert('Hemos experimentado un error. ¡Vuelve pronto!');
         }
 
         const data = await response.json();
 
-        if(data != null) {
+        if (data != null) {
+            debugger
             let countSuscribers = new Map();
+            let countCups = new Map();
 
-            if(Object.keys(data).length > 0) {
-                for(let key in data) {
-                    let {email, saved} = data[key];
+            if (Object.keys(data).length > 0) {
+                for (let key in data) {
+                    let { email, saved, cupsSelect } = data[key];
 
                     let date = saved.split(",")[0];
 
-                    let count = countSuscribers.get(date) || 0;
-                    countSuscribers.set(date, count + 1);
+                    let countS = countSuscribers.get(date) || 0;
+                    let countC = countCups.get(date) || 0;
+                    countSuscribers.set(date, countS + 1);
+                    countCups.set(date, countC + parseInt(cupsSelect, 10));
                 }
             }
             if (countSuscribers.size > 0) {
 
                 subscribers.innerHTML = ''
 
-                for (let [date, count] of countSuscribers) {
+                for (let date of countCups.keys()) {
                     let rowTemplate = `
                         <tr>
                             <th scope="row">1</th>
                             <td>${date}</td>
-                            <td>${count}</td>
+                            <td>${countSuscribers.get(date)}</td>
+                            <td>${countCups.get(date)}</td>
                         </tr>`
                     subscribers.innerHTML += rowTemplate
                 }
             }
         }
-    }catch(error) {
-        alert('Hemos experimentado un error. ¡Vuelve pronto!'); 
+    } catch (error) {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!');
     }
 }
 
 let ready = () => {
     console.log('DOM está listo');
-    debugger
     getData();
 }
 
-let loaded = ( eventLoaded ) => {
+let loaded = (eventLoaded) => {
 
     let myform = document.getElementById('form');
-       
+
     myform.addEventListener('submit', (eventSubmit) => {
-        eventSubmit.preventDefault(); 
-           
+        eventSubmit.preventDefault();
+        const select = document.getElementById('cupsSelect');
+        const selection = select.value;
         const emailElement = document.querySelector('.form-control-lg');
         const emailText = emailElement.value;
 
@@ -105,12 +110,42 @@ let loaded = ( eventLoaded ) => {
                 }
             );
             return;
+        }else if(selection == 0) {
+            select.focus();
+            select.animate(
+                [
+                    { transform: "translateX(0)" },
+                    { transform: "translateX(50px)" },
+                    { transform: "translateX(-50px)" },
+                    { transform: "translateX(0)" }
+                ],
+                {
+                    duration: 400,
+                    easing: "linear",
+                }
+            );
+            return;
         }
-        
+
         sendData();
     });
+}
 
-  }
+document.querySelectorAll('.cm').forEach(element => {
+    let btn = element.querySelector('.cm-btn')
+    element.addEventListener('mouseover', () => {
+        btn.style.transform = 'translate(-50%, -50%) scale(1)';
+    });
+    element.addEventListener('mouseleave', () => {
+        btn.style.transform = 'translate(-50%, -50%) scale(0)';
+    });
+});
+
+document.querySelectorAll('.flip-card').forEach(element => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('flipped');
+    })
+});
 
 window.addEventListener('DOMContentLoaded', ready);
 window.addEventListener('load', loaded);
